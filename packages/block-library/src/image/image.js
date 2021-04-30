4ebe01090b9f0a4b64657d7d6fcf05f008c0a7c1
Aaron Robertshaw <60436221+aaronrobertshaw@users.noreply.github.com>
2021-04-30 16:30:46 +1000

    Pass image border props to image cropper
    This is skipped for the rounded block style.

--- packages/block-library/src/image/image.js
+++ packages/block-library/src/image/image.js
@@ -418,6 +418,7 @@ export default function Image( {
 	}
 
 	const borderProps = useBorderProps( attributes );
+	const isRounded = attributes.className?.includes( 'is-style-rounded' );
 
 	let img = (
 		// Disable reason: Image itself is not meant to be interactive, but
@@ -459,7 +460,7 @@ export default function Image( {
 	if ( canEditImage && isEditingImage ) {
 		img = (
 			<ImageEditor
-				borderProps={ borderProps }
+				borderProps={ isRounded ? undefined : borderProps }
 				url={ url }
 				width={ width }
 				height={ height }
